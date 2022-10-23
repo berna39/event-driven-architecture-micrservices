f36ce34346f8bb2c232840709fac1651f81d0e9e
berna39 <kalemajoseph39@gmail.com>
2022-10-23 14:32:41 +0200

    Service1 ready

--- service1/app.js
+++ service1/app.js
@@ -1,7 +1,39 @@
 const express = require('express');
+const kafka = require('kafka-node');
+const sequelize = require('sequelize');
 const app = express();
+app.use(express.json());
 
-app.listen(4000, (err) => {
+const doRun = () => {
+    const db = new sequelize(producer.env.POSTGRES_URL);
+    const User = db.define('user', {
+        name: sequelize.STRING,
+        email: sequelize.STRING,
+        password: sequelize.STRING
+    });
+
+    const client = new kafka.KafkaClient({kafkaHost: process.env.KAFKA_BOOTSTRAP_SERVER});
+    const producer = new kafka.Producer(client);
+
+    producer.on("ready", async () => {
+
+        app.post('/user', async (req, res) => {
+            producer.send([{topic: process.env.KAFKA_TOPIC, messages: JSON.stringify(req.body)}], async (err, data) => {
+                if(err) console.log(err);
+                else {
+                    const user = await User.create(req.body);
+                    res.send(user);
+                }
+            });
+        });
+
+    });
+}
+
+setTimeout(doRun, 10000);
+
+
+app.listen(process.env.PORT, (err) => {
     if(err) console.log(`Epic Error : ${err}`);
     else console.log(`Service1 up and running`);
 });
--- service2/app.js
+++ service2/app.js
@@ -1,8 +1,17 @@
-
 const express = require('express');
+const kafka = require('kafka-node');
+const mongoose = require('mongoose');
 const app = express();
+app.use(express.json());
 
-app.listen(4000, (err) => {
+const doRun = () => {
+    
+}
+
+setTimeout(doRun, 10000);
+
+
+app.listen(process.env.PORT, (err) => {
     if(err) console.log(`Epic Error : ${err}`);
     else console.log(`Service2 up and running`);
 });
